1589aefa271f7d13e05e89d352ff84f879d88968
sreedharam27 <119181206+sreedharam27@users.noreply.github.com>
2023-01-25 11:01:56 +0530

    pro1-cmt1

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
 provider "aws" {
-region = "ap-south-1"
+region = "us-east-1"
+access_key = "AKIAXJABU3RCW5RVOHDU"
+secrete_key = "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"
 }
 
-resource "aws_instance" "one" {
-  ami             = "ami-0a2457eba250ca23d"
+resource "aws_instance" "Newone" {
+  ami             = "ami-0b5eea76982371e91"
   instance_type   = "t2.micro"
-  key_name        = "terrakeypair"
-  vpc_security_group_ids = [aws_security_group.three.id]
-  availability_zone = "ap-south-1a"
+  key_name        = "USEMEEMBX"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "mouni"
+  }
+}
