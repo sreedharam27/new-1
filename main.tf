2561ffbf6044ffb95cb8a6e462be5ba92ef4e70b
sreedharam27 <119181206+sreedharam27@users.noreply.github.com>
2023-01-26 16:43:02 +0530

    full1 cmmt

---
+++ main.tf
@@ -0,0 +1,65 @@
+provider "aws" {
+region = "ap-south-1"
+}
+
+resource "aws_instance" "one" {
+  ami             = "ami-0cca134ec43cf708f"
+  instance_type   = "t2.micro"
+  key_name        = "new"
+  vpc_security_group_ids = [aws_security_group.three.id]
+  availability_zone = "ap-south-1a"
+  user_data       = <<EOF
+#!/bin/bash
+sudo -i
+yum install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "hai all this is my website created by terraform infrastructurte by mounika server-1" > /var/www/html/index.html
+EOF
+  tags = {
+    Name = "server-1"
+  }
+}
+
+resource "aws_instance" "two" {
+  ami             = "ami-0cca134ec43cf708f"
+  instance_type   = "t2.micro"
+  key_name        = "new"
+  vpc_security_group_ids = [aws_security_group.three.id]
+  availability_zone = "ap-south-1b"
+  user_data       = <<EOF
+#!/bin/bash
+sudo -i
+yum install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "hai all this is my website created by terraform infrastructurte by mounika varma server-2" > /var/www/html/index.html
+EOF
+  tags = {
+    Name = "server-2"
+  }
+}
+
+resource "aws_security_group" "three" {
+  name = "elb-sg"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
