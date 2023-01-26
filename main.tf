2c11edfe235813ef661fa71db92ebe78de531c3a
sreedharam27 <119181206+sreedharam27@users.noreply.github.com>
2023-01-26 11:34:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ access_key = "AKIAXJABU3RCYYL4AUX5"
 secret_key = "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"
 }
 
-resource "aws_instance" "Newone" {
+resource "aws_instance" "newone" {
   ami             = "ami-0b5eea76982371e91"
   instance_type   = "t2.micro"
   key_name        = "USEMEEMBX"
